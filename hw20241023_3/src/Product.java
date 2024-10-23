5e179d0b6d665a3db2885c5a68f342e860746a97
Andrii <ragnarlodbrok.ua@gmail.com>
2024-10-23 10:24:56 +0200

    add class Product

---
+++ hw20241023_3/src/Product.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s - €%.2f", id, name, price);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return id == product.id && Double.compare(price, product.price) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Double.hashCode(price);
+        return result;
+    }
+}
