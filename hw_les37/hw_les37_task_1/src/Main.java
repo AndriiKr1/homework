9c75320cd91503d559dfeaab8bdbbedd6b18cf61
Andrii <ragnarlodbrok.ua@gmail.com>
2024-11-04 14:25:22 +0100

    add method swap

---
+++ hw_les37/hw_les37_task_1/src/Main.java
@@ -0,0 +1,30 @@
+//Andrii Kryshtapovych cohort53
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a" , "Jack");
+        map.put("b", "Micke");
+
+        System.out.println("До swap: " + map);
+swapValue(map, "a", "b");
+        System.out.println("Після swap: " + map);
+
+        swapValue(map, "a", "c");
+        System.out.println("Після swap з відсутнім ключем: " + map);
+
+
+    }
+
+    public static void swapValue(Map<String, String> map, String key1, String key2) {
+
+        map.putIfAbsent(key1, "");
+        map.putIfAbsent(key2, "");
+
+        map.replace(key1, map.put(key2, map.get(key1)));
+    }
+
+}
