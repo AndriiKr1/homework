e4f4b050fb0f75148b2f50edb1ef1c258905ccc9
Andrii <ragnarlodbrok.ua@gmail.com>
2024-11-07 09:38:22 +0100

    add class Auto realization sort

--- hw_les40/hw_les_task1/src/Main.java
+++ hw_les40/hw_les_task1/src/Main.java
@@ -13,19 +13,21 @@ public class Main {
         )
         );
         // по номеру
-        autoList.sort((a1,a2) -> a1.getNumber().compareTo(a2.getNumber()));
+        autoList.sort((a1, a2) -> a1.getNumber().compareTo(a2.getNumber()));
         System.out.println("Sorted by number" + autoList);
 
         //по бренду
-        autoList.sort((a1,a2) -> a1.getBrand().compareToIgnoreCase(a2.getBrand()));
+        autoList.sort((a1, a2) -> a1.getBrand().compareToIgnoreCase(a2.getBrand()));
         System.out.println("\nSorted by Brand:" + autoList);
 
         // по ціні
-        autoList.sort((a1,a2) -> a1.getPrice().compareTo(a2.getPrice()));
+        autoList.sort((a1, a2) -> a1.getPrice().compareTo(a2.getPrice()));
         System.out.println("\nSorted by Price:" + autoList);
 
         // за роком випуску
-        autoList.sort((a1,a2) -> Integer.compare(a1.getYear(), a2.getYear()));
+        autoList.sort((a1, a2) -> Integer.compare(a1.getYear(), a2.getYear()));
         System.out.println("\nSorted by Year:" + autoList);
     }
+
+
 }
