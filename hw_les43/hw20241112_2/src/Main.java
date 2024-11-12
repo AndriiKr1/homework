d246b11c66e952b8f479e4e980c4f169138019db
Andrii <ragnarlodbrok.ua@gmail.com>
2024-11-12 14:58:35 +0100

    add Task2

--- hw_les43/hw20241112_2/src/Main.java
+++ hw_les43/hw20241112_2/src/Main.java
@@ -1,3 +1,14 @@
+/*
+a)  Дан список Programmer(String name, String city, List<Task>  tasks).
+Каждый программист  имеет список задач
+ Task (int number, String description, String status, int daysInProcessing) . Сформировать Map<Boolean, Task> ,
+ где ключу true будут соответствовать задачи находящиеся в обработке (поле daysInProcessing)  менее заданного количества дней, false - остальные
+b)  Дан список Programmer(String name, String city, List<Task>  tasks).
+Каждый программист  имеет список задач Task (int number, String description, String status, int daysInProcessing) .
+Сформировать Map<Integer, Task> , где ключ сколько дней задача в обработке, значение - список задач.
+
+ */
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
