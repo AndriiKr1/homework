867efeb70fb06c08f3729d577388107928c56713
Andrii <ragnarlodbrok.ua@gmail.com>
2024-11-13 10:56:41 +0100

    add method

--- hw_les44/hw2024.11.12_1/src/Main.java
+++ hw_les44/hw2024.11.12_1/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> list = List.of("name:Nick", "length:8", "name:Jack",
+                "name:Ann", "length:6", "name:Johanes");
+        Map<String, String> result = mapParameters(list);
+        System.out.println(result);
+    }
+
+
+    public static Map<String, String> mapParameters(List<String> list) {
+        return list.stream()
+                .map(s->s.split(":"))
+                .collect(Collectors.groupingBy(
+                        arr->arr[0],
+                        Collectors.mapping(
+                                arr->arr[1],
+                                Collectors.joining(",")
+                        )
+                ));
     }
 }
